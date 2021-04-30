ba127251ea08f6439635d3f0fef2880adcc5e72a
Maxmustermen <73706106+Maxmustermen@users.noreply.github.com>
2021-04-30 12:21:30 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function addSeite()
+{
+
+    var link = document.getElementById("link");
+    var pictureLink = document.getElementById("pictureLink");
+    //var Seite = new Array [websiteName, link, pictureLink];
+    var websiteName = document.createAttribute(document.getElementById("websiteName"));
+    document.body.appendChild(websiteName);
+}
+function getWebsiteName()
+{
+    
+}
